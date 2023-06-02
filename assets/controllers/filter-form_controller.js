49d7ec82282161ddace31c01482eda53578ba8b4
Jose J. Matarín <jmatarin@grupovertice.com>
2023-06-02 14:42:13 +0200

    Inicial

--- assets/controllers/filter-form_controller.js
+++ assets/controllers/filter-form_controller.js
@@ -3,20 +3,12 @@ import { useDispatch } from 'stimulus-use';
 
 export default class extends Controller {
 
-    connect() {
-
-    }
-
     onSubmit(event) {
         event.preventDefault();
 
         fetch(this.element.action, {
             method: this.element.method,
             body: new URLSearchParams(new FormData(this.element))
-        })
-            .then(function(response) {
-                this.dispatch("filter:success");
-                //response.text()
-            });
+        }).then(() => this.dispatch("success"));
     }
 }
